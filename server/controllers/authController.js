0bd2f84f5fb1e1f5078c260f320219f060cd64a1
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-21 12:22:32 -0300

    login api added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,3 +21,24 @@ export const register = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id, user.role),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
