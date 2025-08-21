612a57c7662c26679b1765e28e6c2129373aea38
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-21 12:28:35 -0300

    auth routed successfully for login and register

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 
 const PORT = process.env.PORT || 5000;
