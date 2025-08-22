a313c5492dbc31cd0a55516934b00881d946dbcc
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-22 01:33:16 -0300

    getAllMessages api added

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -11,3 +11,8 @@ export const getAllConversations = async (req, res) => {
   const conversations = await Conversation.find().populate("participants", "name email");
   res.json(conversations);
 };
+
+export const getAllMessages = async (req, res) => {
+  const messages = await Message.find().populate("sender", "name email");
+  res.json(messages);
+};
