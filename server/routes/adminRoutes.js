2d8ba51afa0ad88d2b2f888ab700da18e9424af6
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-22 01:36:46 -0300

    adminrouted successfully

---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { adminOnly, protect } from "../middleware/auth.js";
+import { getAllUsers, getAllConversations, getAllMessages } from "../controllers/adminController.js";
+
+const router = express.Router();
+
+router.get("/users", protect, adminOnly, getAllUsers);
+router.get("/conversations", protect, adminOnly, getAllConversations);
+router.get("/messages", protect, adminOnly, getAllMessages);
+
+export default router;
