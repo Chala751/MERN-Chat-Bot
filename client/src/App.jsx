8d5ebc674f02a2a14304322ec8b9eb0d1b3ba324
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-23 11:22:33 -0300

    routed success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/chat"
+        element={
+          <ProtectedRoute>
+            <Chatbot />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute adminOnly>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   )
 }
 
