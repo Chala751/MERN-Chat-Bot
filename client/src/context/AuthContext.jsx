0f25bcab9724147bd689e642f5c0c5b5ddefadbe
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-23 11:52:07 -0300

    auth context added

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const navigate = useNavigate();
+
+  const login = (userData) => {
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+    navigate("/chat");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
