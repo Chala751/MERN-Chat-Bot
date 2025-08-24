431468f74a55231c11afef92cfaffa502c68b3dd
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-24 06:26:40 -0300

    utils added

---
+++ client/src/utils/socket.js
@@ -0,0 +1,7 @@
+// For real-time updates (future enhancement)
+// This is a placeholder in case you add socket.io
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+export default socket;
